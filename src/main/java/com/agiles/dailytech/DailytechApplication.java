e3634235946ae98b6283326eabb6d3f8b9843850
Sajid <yeamimhossainsajid@gmail.com>
2025-01-13 22:03:32 +0600

    first commit

---
+++ src/main/java/com/agiles/dailytech/DailytechApplication.java
@@ -0,0 +1,13 @@
+package com.agiles.dailytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailytechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailytechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agiles/dailytech/DailytechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agiles.dailytech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailytechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
