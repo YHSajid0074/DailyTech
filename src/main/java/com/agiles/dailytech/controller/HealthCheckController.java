b59a961423041967ebf75e680eeb984c18a662eb
Sajid <yeamimhossainsajid@gmail.com>
2025-01-21 00:29:09 +0600

    .

--- src/main/java/com/agiles/dailytech/controller/HealthCheckController.java
+++ src/main/java/com/agiles/dailytech/controller/HealthCheckController.java
@@ -3,6 +3,7 @@ package com.agiles.dailytech.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class HealthCheckController {
     @GetMapping("/healthcheck")
